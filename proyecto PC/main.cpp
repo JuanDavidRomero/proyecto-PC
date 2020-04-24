0c7b853d26642d9d1551a55f95ab9b4fa1196764
David Romero <davidbouken@hotmail.com>
2020-04-23 20:46:43 -0500

    First Commit

--- proyecto PC/main.cpp
+++ proyecto PC/main.cpp
@@ -8,8 +8,6 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+    printf("hellotha");
 }
