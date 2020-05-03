602b2be374961a93a8fe4037a47da7395b15b3d6
David Romero <davidbouken@hotmail.com>
2020-05-02 19:12:24 -0500

    se incluye menu

--- proyecto PC/main.cpp
+++ proyecto PC/main.cpp
@@ -7,7 +7,53 @@
 //
 
 #include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+void addHoja(){
+    cout <<"hola"<<endl;
+}
+
+void generarR(){}
+
+void reclamarR(){}
 
 int main() {
-    printf("hellotha");
+    char opcion;
+    bool fin= true;
+    while(fin == true){
+        cout<<"SUPER CALCULOS S.A."<<endl;
+        cout<<"------------------------------"<<endl;
+        cout<<"A = ingresar nueva hoja de calculo"<<endl;
+        cout<<"B = generar reporte"<<endl;
+        cout<<"C = reclamar reporte"<<endl;
+        cout<<"E = salir"<<endl;
+        cout<<"------------------------------"<<endl;
+        cout<<endl;
+        cout<<"seleccione"<<endl;
+        cin >>opcion;
+        switch (opcion){
+            case 'A':
+            case 'a':
+                addHoja();
+                break;
+            case 'B':
+            case 'b':
+                generarR();
+                break;
+            case 'C':
+            case 'c':
+                reclamarR();
+                break;
+            case 'E':
+            case 'e':
+                fin = false;
+                break;
+            default:
+                break;
+        }
+    }
 }
