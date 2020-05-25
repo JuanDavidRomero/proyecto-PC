84495c50053a7f3b5410e6a751c04497b6174a24
Santiago Barbosa <santibar15@hotmail.com>
2020-05-25 16:34:55 -0500

    Revert "Update pa ver si se arregla :,v "
    This reverts commit 7ae5e1a4d09be199dd9541619272bc7b98d29858.

--- proyecto PC/main.cpp
+++ proyecto PC/main.cpp
@@ -273,9 +273,7 @@ void guardarAr(Nodo<sHoja> *libro){
 
     while(currentNode != NULL){
         arHoja<<currentNode->dato.columnasH<<" "<<currentNode->dato.filasH<<endl;
-
         for(int i = 0; i < currentNode->dato.filasH; i++){
-
             for(int j = 0; j < currentNode->dato.columnasH; j++){
                     if((*(*(currentNode->dato.celdas+i)+j)).valorNumerico == 0)
                     {
